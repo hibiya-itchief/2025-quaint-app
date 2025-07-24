be29ce9f65bfd758fbff84a63dad81fa4e59c2a7
MRharu0310 <sharuharu0310@gmail.com>
2025-07-24 16:31:13 +0000

    2025サイト準備中を知らせるページ

--- app/plugins/vuetify.ts
+++ app/plugins/vuetify.ts
@@ -2,11 +2,18 @@
 import { createVuetify } from "vuetify";
 import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
 import colors from "vuetify/util/colors";
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    // ... your configuration
+    icons: {
+      defaultSet: "mdi",
+      aliases,
+      sets: {
+        mdi,
+      },
+    },
   });
   app.vueApp.use(vuetify);
 });
